1a572e8ad22b6619581667d062331a3ab4ec26e7
Baron Roberts <baron@cthing.com>
2024-02-14 00:15:54 -0800

    Use replacement for google collections.

--- build.gradle.kts
+++ build.gradle.kts
@@ -36,11 +36,11 @@ dependencies {
     implementation(libs.jsr305)
 
     spotbugsPlugins(libs.spotbugsContrib)
-}
 
-configurations.checkstyle {
-    resolutionStrategy.capabilitiesResolution.withCapability("com.google.collections:google-collections") {
-        select("com.google.guava:guava:0")
+    modules {
+        module("com.google.collections:google-collections") {
+            replacedBy("com.google.guava:guava", "google-collections is now part of Guava")
+        }
     }
 }
 
